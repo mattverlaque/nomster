460a617771d72d98c54acab44a79cc32cc905c48
Matt Verlaque <matt@runyourgym.com>
2016-12-13 06:21:45 +0000

    Initiate slider

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -23,7 +23,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-   process resize_to_fill: [500, 500]
+ 
+
+  process resize_to_fill: [1240, 720]
 
   #
   # def scale(width, height)
